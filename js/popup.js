957a6937747e5c94b7edc5b41c21377ee6b0ef5b
Jesse Evers <jesse@jesseevers.com>
2020-03-11 02:05:34 -0400

    Moved all JS files, added Industry field and styling to popup.html

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $('#id-submit').click(function(e) {
-    // e.preventDefault();    
+    e.preventDefault();
     const formData = new FormData($(this).parent().get(0));
-    
+
     if (formData.get('organization') && formData.get('url') && formData.get('description')) {
-      
+      // To be filled in. Currently uploading so that I can get app ID for OAuth.
     }
   });
 });
